8a1b079c1538053ab638dc9ce47ca9b0aab92ce1
Saagar Takhi <stakhi08@gmail.com>
2020-12-15 06:08:03 +0530

    [chat-app]: chore(app): adds Suspense support

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { SideNav } from './components/side-nav';
 import { ChatWindow } from './components/chat-window';
 import { MessagesProvider, UsersProvider } from '../providers';
 
 const Chat = () => (
   <div className="container h-screen flex items-center justify-center bg-gray-200">
-    <UsersProvider>
-      <MessagesProvider>
-        <div className="flex h-5/6 w-5/6 border-2 border-gray-300 rounded-2xl">
-          <div className="w-3/12">
-            <SideNav />
+    <Suspense fallback="Loading...">
+      <UsersProvider>
+        <MessagesProvider>
+          <div className="flex h-5/6 w-5/6 border-2 border-gray-300 rounded-2xl">
+            <div className="w-3/12">
+              <SideNav />
+            </div>
+            <div className="flex-1">
+              <ChatWindow />
+            </div>
           </div>
-          <div className="flex-1">
-            <ChatWindow />
-          </div>
-        </div>
-      </MessagesProvider>
-    </UsersProvider>
+        </MessagesProvider>
+      </UsersProvider>
+    </Suspense>
   </div>
 );
 
